ffce5cd650cfe64385934bbb3ac6d799530c23bc
Juan Cuenca <juan99.cuenca@gmail.com>
2024-09-24 15:22:08 -0400

    refactor activations file

--- src/nn/activations.rs
+++ src/nn/activations.rs
@@ -1,90 +1,100 @@
 use crate::matrix::Matrix;
 use super::sequential::Layer;
 
-pub struct ReLU {
-    cached_input: Option<Matrix>,
-}
+pub trait ActivationFunction {
+    fn activate(x: f64) -> f64;
+    fn derivative(x: f64) -> f64;
 
-impl ReLU {
-    pub fn new() -> ReLU {
-        ReLU { 
-            cached_input: None 
-        }
-    }
-
-    pub fn activate(x: f64) -> f64 {
-        if x <= 0.0 { 0.0 } else { x }  
-    }
-
-    pub fn relu_derivative(x: f64) -> f64 {
-        if x == 0.0 { 0.0 } else { 1.0 }
-    }
-
-    pub fn forward(&mut self, input: &Matrix) -> Matrix {
-        self.cached_input = Some(input.clone());
-
+    fn apply(input: &Matrix) -> Matrix {
         input.map(|x| Self::activate(x))
     }
 
-    pub fn backward(&mut self, grad_output: &Matrix) -> Matrix {
-        let input = self.cached_input.as_ref().expect("No cached input found. Did you forget to call forward()?");
-        let grad_input = input.map(|x| Self::relu_derivative(x));
+    fn compute_gradient(input: &Matrix, grad_output: &Matrix) -> Matrix {
+        let grad_input = input.map(|x| Self::derivative(x));
         
         grad_input.hadamard_product(grad_output)
     }
 }
 
-impl Layer for ReLU {
+pub struct Relu {
+    cached_input: Option<Matrix>,
+}
+
+impl Relu {
+    pub fn new() -> Relu {
+        Relu { 
+            cached_input: None 
+        }
+    }
+}
+
+impl ActivationFunction for Relu {
+    fn activate(x: f64) -> f64 {
+        if x > 0.0 { 
+            x 
+        } else { 
+            0.0 
+        }  
+    }
+
+    fn derivative(x: f64) -> f64 {
+        if x > 0.0 { 
+            1.0 
+        } else { 
+            0.0 
+        }
+    }
+}
+
+impl Layer for Relu {
     fn forward(&mut self, input: &Matrix) -> Matrix {
-        self.forward(input)
+        self.cached_input = Some(input.clone());
+
+        Self::apply(input)
     }
 
     fn backward(&mut self, grad_output: &Matrix) -> Matrix {
-        self.backward(grad_output)
+        let input = self.cached_input.as_ref().expect("No cached input found. Did you forget to call forward()?");
+
+        Self::compute_gradient(input, grad_output)
     }
 }
 
 pub struct Sigmoid {
-    cached_output: Option<Matrix>
+    cached_input: Option<Matrix>
 }
 
 impl Sigmoid {
     pub fn new() -> Sigmoid {
         Sigmoid {
-            cached_output: None
+            cached_input: None
         }
     }
-    
-    pub fn activate(x: f64) -> f64 {
+}
+ 
+impl ActivationFunction for Sigmoid {
+    fn activate(x: f64) -> f64 {
         1.0 / (1.0 + (-x).exp())
     }
-
-    fn forward(&mut self, input: &Matrix) -> Matrix {
-        let output = input.map(|x| Self::activate(x));
-        self.cached_output = Some(output.clone());
+    
+    fn derivative(x: f64) -> f64 {
+        let s = Self::activate(x);
         
-        output
-    }
-
-    fn sigmoid_derivative(output: f64) -> f64 {
-        output * (1.0 - output)
-    }
-
-    pub fn backward(&mut self, grad_output: &Matrix) -> Matrix {
-        let output = self.cached_output.as_ref().expect("No cached output found. Did you forget to call forward()?");
-        let grad_input = output.map(|o| Self::sigmoid_derivative(o));
-        
-        grad_input.hadamard_product(grad_output)
+        s * (1.0 - s)
     }
 }
 
 impl Layer for Sigmoid {
     fn forward(&mut self, input: &Matrix) -> Matrix {
-        self.forward(input)
+        self.cached_input = Some(input.clone());
+
+        Self::apply(input)
     }
 
     fn backward(&mut self, grad_output: &Matrix) -> Matrix {
-        self.backward(grad_output)
+        let input = self.cached_input.as_ref().expect("No cached input found. Did you forget to call forward()?");
+
+        Self::compute_gradient(input, grad_output)
     }
 }
 
@@ -93,8 +103,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_activation_sigmoid_success() {
+    fn test_sigmoid_success() {
         assert_eq!(Sigmoid::activate(0.0), 0.5);
-        assert_eq!(Sigmoid::activate(0.5), 0.6224593312018546);
+    }
+
+    #[test]
+    fn test_sigmoid_derivative_success() {
+        assert_eq!(Sigmoid::derivative(0.0), 0.25);   
+    }
+
+    
+    #[test]
+    fn test_relu_success() {
+        assert_eq!(Relu::activate(3.0), 3.0);
+        assert_eq!(Relu::activate(-1.0), 0.0);
+    }
+
+    #[test]
+    fn test_relu_derivative_success() {
+        assert_eq!(Relu::derivative(3.0), 1.0);
+        assert_eq!(Relu::derivative(-1.0), 0.0);   
     }
 }
