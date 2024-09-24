b6c290501abae849e29b398d8bcd493dfd72a36b
Juan Cuenca <juan99.cuenca@gmail.com>
2024-09-24 13:17:40 -0400

    add relu activation function

--- src/nn/activations.rs
+++ src/nn/activations.rs
@@ -1,6 +1,49 @@
 use crate::matrix::Matrix;
 use super::sequential::Layer;
 
+pub struct ReLU {
+    cached_input: Option<Matrix>,
+}
+
+impl ReLU {
+    pub fn new() -> ReLU {
+        ReLU { 
+            cached_input: None 
+        }
+    }
+
+    pub fn activate(x: f64) -> f64 {
+        if x <= 0.0 { 0.0 } else { x }  
+    }
+
+    pub fn relu_derivative(x: f64) -> f64 {
+        if x == 0.0 { 0.0 } else { 1.0 }
+    }
+
+    pub fn forward(&mut self, input: &Matrix) -> Matrix {
+        self.cached_input = Some(input.clone());
+
+        input.map(|x| Self::activate(x))
+    }
+
+    pub fn backward(&mut self, grad_output: &Matrix) -> Matrix {
+        let input = self.cached_input.as_ref().expect("No cached input found. Did you forget to call forward()?");
+        let grad_input = input.map(|x| Self::relu_derivative(x));
+        
+        grad_input.hadamard_product(grad_output)
+    }
+}
+
+impl Layer for ReLU {
+    fn forward(&mut self, input: &Matrix) -> Matrix {
+        self.forward(input)
+    }
+
+    fn backward(&mut self, grad_output: &Matrix) -> Matrix {
+        self.backward(grad_output)
+    }
+}
+
 pub struct Sigmoid {
     cached_output: Option<Matrix>
 }
