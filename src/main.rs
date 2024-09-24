4affd6f8372c76d78a0334f7dee29bd4cc83371d
Juan Cuenca <juan99.cuenca@gmail.com>
2024-09-24 18:07:12 -0400

    add model v0

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
 use rustnet::nn::{
-    loss::{
-        MeanAbsoluteError,
-        MeanSquaredError, 
-        LossFunction
-    },
-    activations::Sigmoid, 
-    linear::Linear, 
-    sequential::Sequential
+    activations::Sigmoid, linear::Linear, loss::{
+        LossFunction, MeanAbsoluteError, MeanSquaredError
+    }, sequential::Sequential
 };
 use rustnet::matrix::Matrix;
 
+pub struct ModelV0 {
+    sequential: Sequential,
+}
+
+impl ModelV0 {
+    pub fn new(input_size: usize, hidden_units: usize, output_size: usize, seed: Option<u64>) -> ModelV0 {
+        let mut sequential = Sequential::new();
+        sequential.add_layer(Box::new(Linear::new(input_size, hidden_units, seed)));
+        sequential.add_layer(Box::new(Sigmoid::new()));
+        sequential.add_layer(Box::new(Linear::new(hidden_units, output_size, seed)));
+        sequential.add_layer(Box::new(Sigmoid::new()));
+
+        ModelV0 { 
+            sequential 
+        }
+    }
+
+    pub fn train<F: LossFunction>(&mut self, train_x: &Matrix, train_y: &Matrix, epochs: usize, _loss_function: F) {
+        for epoch in 0..epochs {
+            let predictions = self.sequential.forward(&train_x);    
+            let loss = F::calculate(&train_y, &predictions);
+        
+            let loss_grad = F::gradient(&train_y, &predictions);
+            
+            self.sequential.backward(&loss_grad);
+            
+            if epoch % (epochs / 10) == 0 {
+                println!("Epoch {}: Loss = {}", epoch, loss);
+            }
+        }
+    }
+
+    pub fn predict(&mut self, train_x: &Matrix) -> Matrix {
+        self.sequential.forward(&train_x)
+    }
+}
+
+
+
 fn main() {
-    let seed = Some(42);
-
-    let mut sequential = Sequential::new();
-    sequential.add_layer(Box::new(Linear::new(2, 2, seed)));
-    sequential.add_layer(Box::new(Sigmoid::new()));
-    sequential.add_layer(Box::new(Linear::new(2, 1, seed)));
-    sequential.add_layer(Box::new(Sigmoid::new()));
-
     let train_x = Matrix::new((4, 2), vec![
         0.0, 0.0,
         0.0, 1.0,
@@ -33,20 +59,12 @@ fn main() {
         1.0,
     ]);
 
-    let num_epochs = 5000;
+    let mut ModelV0 = ModelV0::new(2, 2, 1,Some(42));
 
-    for epoch in 0..num_epochs {
-        let predictions = sequential.forward(&train_x);
-        let loss = MeanSquaredError::calculate(&train_y, &predictions);
-        
-        let loss_grad = MeanSquaredError::gradient(&train_y, &predictions); 
-        sequential.backward(&loss_grad);
-        
-        if epoch % 500 == 0 {
-            println!("Epoch {}: Loss = {}", epoch, loss);
-        }
-    }
+    ModelV0.train(&train_x, &train_y, 5000, MeanSquaredError);
 
-    let predictions = sequential.forward(&train_x);
-    println!("{predictions:#?}");
+    ModelV0.train(&train_x, &train_y, 5000, MeanAbsoluteError);
+
+    // let predictions = ModelV0.predict(&train_x);
+    // println!("{predictions:#?}");
 }
